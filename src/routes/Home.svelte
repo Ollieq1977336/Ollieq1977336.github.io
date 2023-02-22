4d4b95b49f31778769de7ecc4005a0842c110e11
Ollieq1977336 <olliequayle08@icloud.com>
2023-02-22 15:21:56 +0000

    Initial commit

---
+++ src/routes/Home.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+   import { formatNumber } from "../gamelogic/utils";
+   import MoneyButton from "../components/MoneyButton.svelte";
+   import { gameModel } from "../gamelogic/gamemodel";
+   import GeneratorList from "../components/GeneratorList.svelte";
+</script>
+
+<h1>Coding Company Simulator!</h1>
+<p>
+   Goal: Get the most amount of money possible!
+</p>
+<p>
+   The amount of Pounds you have is:
+   <strong>£{formatNumber($gameModel.saveData.money, 2)}</strong>
+</p>
+<MoneyButton />
+<div>
+   <GeneratorList />
+</div>
+
+<style>
+   div {
+      display: grid;
+      grid-template-columns: repeat(1, 100%);
+   }
+
+   @media screen and (min-width: 992px) {
+      div {
+         grid-template-columns: repeat(2, 50%);
+      }
+   }
+</style>
---
+++ src/routes/Options.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+   import { gameModel } from "../gamelogic/gamemodel";
+   import { sendMessage } from "../gamelogic/notifications";
+   import { resetSaveGame, saveSaveGame } from "../gamelogic/saveloadfunctions";
+
+   /**
+    * On click action to save the game in localstorage
+    */
+   function saveGame() {
+      saveSaveGame($gameModel.saveData);
+      sendMessage("Your data has been saved!");
+   }
+
+   /**
+    * On click action to reset the save game in localstorage
+    */
+   function resetGame() {
+      if (confirm("Are you REALLY Sure you want to reset all data? (IF you do, you can't get it back!")) {
+         resetSaveGame();
+         sendMessage("Save Data has been wiped");
+      }
+   }
+</script>
+
+<h1>Options</h1>
+
+<button on:click={saveGame}>Save All Stats</button>
+
+<button on:click={resetGame} style="background-color: red;">RESET All Data</button>
+
+<style>
+</style>
